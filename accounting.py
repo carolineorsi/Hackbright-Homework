536676360349528717579cb441e483850f8f1e3f
Caroline Orsi <caroline.orsi@gmail.com>
2014-10-02 22:25:19 -0700

    Creates customer list and creates list of those who underpaid.

--- accounting.py
+++ accounting.py
@@ -1,5 +1,5 @@
 def main():
-    check_for_underpaid()
+    print_underpaid_report("customer_orders.csv")
  
 def create_customer_list(filename):
 
@@ -14,17 +14,27 @@ def create_customer_list(filename):
     return customer_list
 
 
-def check_for_underpaid():
+def check_for_underpaid(filename):
 
     underpaid = []
 
-    customers = create_customer_list("customer_orders.csv")
+    customers = create_customer_list(filename)
 
     for customer in customers:
         if float(customer[2]) != float(customer[3]):
             underpaid.append(customer)
 
     print underpaid
+
+def print_underpaid_report(filename):
+
+    underpaid = check_for_underpaid(filename)
+
+    for customer in underpaid:
+        name = customer[1]
+        amount_due = float(customer[2])
+        price_paid = float(customer[3])
+        print "%s paid %.2f, expected %.2f" % (name, price_paid, amount_due)
 
 
 
