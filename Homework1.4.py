068183e1f69a9fd94be1087459885882a73f0249
Caroline Orsi <caroline.orsi@gmail.com>
2014-10-03 12:20:11 -0700

    Working solution as described in class

--- Homework1.4.py
+++ Homework1.4.py
@@ -1,30 +1,43 @@
+MELON_COST = 1.00
+
 def main():
-    print_underpaid_report("customer_orders.csv")
+
+    customer_data = open("customer_orders.csv")
+
+    for customer in customer_data:
+        cid, name, num_melons, amt_paid = customer.strip().split(',')
+        amt_owed = int(num_melons) * MELON_COST
+        amt_paid = float(amt_paid)
+
+        if amt_paid != amt_owed:
+            print "%s paid $%.2f, expected $%.2f" % (name, amt_paid, amt_owed)
+
+    customer_data.close()
  
 
-def create_customer_list(filename):
+# def create_customer_list(filename):
 
-    customer_list = []
+#     customer_list = []
 
-    f = open(filename)
-    for line in f:
-        customer_info = line.split(",")
-        customer_list.append(customer_info)
-    f.close()
+#     f = open(filename)
+#     for line in f:
+#         customer_info = line.split(",")
+#         customer_list.append(customer_info)
+#     f.close()
 
-    return customer_list
+#     return customer_list
 
 
-def print_underpaid_report(filename):
+# def print_underpaid_report(filename):
 
-    customers = create_customer_list(filename)
+#     customers = create_customer_list(filename)
 
-    for customer in customers:
-        name = customer[1]
-        amount_due = float(customer[2])
-        price_paid = float(customer[3])        
-        if amount_due > price_paid:
-            print "%s paid $%.2f, expected $%.2f" % (name, price_paid, amount_due)
+#     for customer in customers:
+#         name = customer[1]
+#         amount_due = float(customer[2])
+#         price_paid = float(customer[3])        
+#         if amount_due > price_paid:
+#             print "%s paid $%.2f, expected $%.2f" % (name, price_paid, amount_due)
 
 
 if __name__ == "__main__":
